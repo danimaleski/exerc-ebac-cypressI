452b6b0b694969831d6f94648f6f8f4421961f7c
Daniella Maleski <daniella.maleski@gmail.com>
2025-06-23 20:59:24 -0300

    actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig ({
+module.exports = defineConfig({
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Você pode adicionar eventos personalizados aqui, se necessário
     },
   },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "mochawesome-report",
+    overwrite: false,
+    reportFilename: "index.html",
+    html: true,
+    json: false
+  }
 });
